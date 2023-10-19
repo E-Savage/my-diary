989220e389610941439852f5570e2b2f40a25961
Eric Savage <eric.n.savage2000@gmail.com>
2023-10-19 16:27:25 -0400

    changed settings and added new stuff to some files

--- entries/admin.py
+++ entries/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Entry
 
 # Register your models here.
+
+admin.site.register(Entry) # must register to see this on the django admin site
--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models # gets model class to inherit for our models
+from django.utils import timezone # timezone is a datafield used to mark time of creation
 
 # Create your models here.
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+
+    # equivalent of to_string()
+    def __str__(self): 
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Entries"
